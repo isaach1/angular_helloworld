ff9e9e120d75c48357f6e8776420e74a2b02b1f4
isaach1 <isaachudis1@gmail.com>
2020-06-13 15:55:12 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewPageComponent } from './new-page/new-page.component';
+import { RouterModule } from '@angular/router';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPageComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FileUploadModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'new-page', component: NewPageComponent },
+      { path: 'file-upload', component: FileUploadComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent implements OnInit {
+
+  uploader: FileUploader;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
